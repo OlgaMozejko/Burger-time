275ff24b138545a899178190cb08f44b48a89c90
OlgaMozejko <78202579+OlgaMozejko@users.noreply.github.com>
2024-05-23 22:29:08 +0200

    cleanup

--- burger-project/src/pages/_app.tsx
+++ burger-project/src/pages/_app.tsx
@@ -2,16 +2,10 @@ import { AppProps } from 'next/app';
 import { wrapper } from '@/store';
 import "@/styles/globals.scss";
 import Header from '@/layout/header/header';
-import { useStore } from 'react-redux';
-import { useEffect } from 'react';
 import MetaData from '@/layout/metaData/metaData';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const store = useStore();
 
-  useEffect(() => {
-    console.log("Client-side store state:", store.getState());
-  }, [store]);
 
   return (
     <>
